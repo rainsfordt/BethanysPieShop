f4641237b52c425992751f3ccbe1d3d3d8b94f70
rainsfordt <tmrainsford@gmail.com>
2022-06-22 13:53:02 +0100

    Completed Section 8 - Creating Order Form

--- BethanysPieShop/GlobalUsings.cs
+++ BethanysPieShop/GlobalUsings.cs
@@ -3,3 +3,5 @@ global using BethanysPieShop.Models;
 global using BethanysPieShop.ViewModels;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Razor.TagHelpers;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.AspNetCore.Mvc.ModelBinding;
